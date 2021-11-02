ad3ad134eb61706f476753122590deb4effff3c2
Dylan K <drkunkel7@gmail.com>
2021-11-02 15:45:23 -0500

    added Regular Warehouse to projects array

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Navigation from './components/Navigation';
+// import Navigation from './components/Navigation';
 import Contact from './components/Contact';
 import Project from './components/Project';
 import About from './components/About';
@@ -14,6 +14,7 @@ function App() {
   const [currentNav, setCurrentNav] = useState("home");
 
   const [projects] = useState([
+    { title: 'Regular Warehouse', image: 'RegWarehouseScreenshot', link: 'https://regular-warehouse.herokuapp.com/warehouse', repo: 'https://github.com/rrcampbell-exe/regular-warehouse'},
     { title: 'Run Buddy', image: 'RunBuddyScreenshot', link: "https://dkunk7.github.io/run-buddy/", repo: "https://github.com/Dkunk7/run-buddy"},
     { title: 'Advice from a Pirate', image: 'PirateAdviceScreenshot', link: 'https://kait-weishaar.github.io/Pirate-Advice/', repo: "https://github.com/kait-weishaar/Pirate-Advice"},
     { title: 'Volley-All', image: 'VolleyAllScreenshot', link: "https://volley-all.herokuapp.com/login", repo: "https://github.com/rrcampbell-exe/volley-all"}
